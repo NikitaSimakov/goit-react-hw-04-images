34e33ff90cf3a2447d869af9677cef264cae2d19
NikitaSimakov <mishaaniskin@gmail.com>
2023-05-23 20:02:29 +0300

    Update ImageGallery.js

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getSearchImages } from 'api/SearchImageApi';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
