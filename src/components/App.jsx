40d0659079ed7695c89ed4375c996f4edd5b5c3c
NikitaSimakov <mishaaniskin@gmail.com>
2023-05-25 08:25:04 +0300

    Cleaning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import { Searchbar } from "./Searchbar/Searchbar";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Button } from "./Button/Button";
 import { Modal } from "./Modal/Modal";
-// import { Loader } from "./Loader/Loader";
 import css from './Styles.module.css'
 
 export const App = () => {
@@ -11,7 +10,6 @@ export const App = () => {
   const [page, setPage] = useState(1);
   const [largeImg, setLargeImg] = useState('');
   const [showModal, setShowModal] = useState(false);
-  // const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
   const [isHidden, setIsHidden] = useState(true);
 
@@ -25,17 +23,7 @@ export const App = () => {
     setLargeImg(link);
     handleShowModalToggle();
 }
-// const memoizedCallback = useCallback(
-//   () => {
-//     doSomething(a, b);
-//   },
-//   [a, b],
-// );
-// const handleLoading = () => setIsLoading(prevState=>!prevState)
-// const handleLoading = (bool) => setIsLoading(bool)
-// const handleLoading = useCallback(
-//   () => {setIsLoading(prevState=>!prevState)}, [isLoading]
-// )
+
   const handleGetError = (error) => setError(error)
   const handleButtonHide = (bool) => setIsHidden(bool)
         return (
@@ -43,52 +31,9 @@ export const App = () => {
           <Searchbar onSubmit={handleFormSubmit}/>
           <ImageGallery searchQuery={value} page={page} getLargeImg={handleGetLargeImg} handleGetError={handleGetError} handleButtonHide={handleButtonHide}/>
           {error && <h2 className={css.error_message}>Something went wrong..</h2>}
-          {/* {isLoading && <Loader />} */}
           {!isHidden && <Button onClick={handleButtonClick}/>}
           {showModal && <Modal largeImgLink={largeImg} onKeydown={handleShowModalToggle}/>}
         </div>
       );
   
 }
-// export class App extends Component  {
-//   state = {
-//     value: '',
-//     page: 1,
-//     largeImg: '',
-//     showModal: false,
-//     isLoading: false,
-//     error: false,
-//     isHidden: true,
-//   }
-
-  // handleButtonClick = () => {
-  //   this.setState(prevState => {
-  //     return {page: prevState.page + 1}
-  //   })};
-
-//   handleFormSubmit = (value) => this.setState({value, page: 1})
-
-//   handleShowModalToggle = () => this.setState(({showModal}) => ({showModal: !showModal}))
-  
-//   handleGetLargeImg = (link) => {
-// this.setState({largeImg: link})
-// this.handleShowModalToggle();
-//   }
-
-//   handleLoading = () => this.setState(({isLoading}) => ({isLoading: !isLoading}))
-//   handleGetError = (error) => this.setState({error: error})
-//   handleButtonHide = (bool) => this.setState({isHidden: bool})
-//   render () {
-//     const {page, value, largeImg, showModal, isLoading, error, isHidden} = this.state;
-//     const {handleFormSubmit,handleGetLargeImg,handleLoading,handleButtonClick,handleShowModalToggle,handleGetError, handleButtonHide} = this
-//     return (
-//     <div className={css.App}>
-//       <Searchbar onSubmit={handleFormSubmit}/>
-//       <ImageGallery searchQuery={value} page={page} getLargeImg={handleGetLargeImg} isLoading={handleLoading} handleGetError={handleGetError} handleButtonHide={handleButtonHide}/>
-//       {error && <h2 className={css.error_message}>Something went wrong..</h2>}
-//       {isLoading && <Loader />}
-//       {!isHidden && <Button onClick={handleButtonClick}/>}
-//       {showModal && <Modal largeImgLink={largeImg} onKeydown={handleShowModalToggle}/>}
-//     </div>
-//   );}
-// };
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -51,70 +51,11 @@ export const ImageGallery = ({
     </>
   );
 };
-// export class ImageGallery extends Component {
-//   state = {
-//     gallery: [],
-//     error: '',
-//   };
-
-// async componentDidUpdate(prevProps) {
-//   const { searchQuery, page, isLoading, handleButtonHide, handleGetError } =
-//     this.props;
-
-//   try {
-//     if (prevProps.searchQuery !== searchQuery) {
-//       this.setState({ gallery: [] });
-//       handleGetError(false);
-//       isLoading();
-//       const gallery = await getSearchImages(searchQuery, page);
-//       this.setState({ gallery: gallery.hits });
-//       handleButtonHide(false);
-//       if (gallery.hits.length < 12) {
-//         handleButtonHide(true);
-//         handleGetError(false);
-//       }
-//       if (gallery.hits.length >= 12) handleButtonHide(false);
-//       isLoading();
-//       if (gallery.hits.length === 0) {
-//         handleGetError(true);
-//         handleButtonHide(true);
-//         return;
-//       }
-//     }
-//     if (prevProps.page < page) {
-//       isLoading();
-//       const gallery = await getSearchImages(searchQuery, page);
-//       this.setState(prevState => {
-//         return { gallery: [...prevState.gallery, ...gallery?.hits] };
-//       });
-
-//       isLoading();
-//       return;
-//     }
-//   } catch (error) {
-//     isLoading();
-//     if (error) return handleGetError();
-//   }
-// }
-
-//   render() {
-//     const { gallery } = this.state;
-//     const { getLargeImg } = this.props;
-//     return (
-//       <>
-//         <ul className={css.ImageGallery}>
-//           <ImageGalleryItem gallery={gallery} getLargeImg={getLargeImg} />
-//         </ul>
-//       </>
-//     );
-//   }
-// }
 
 ImageGallery.propTypes = {
   searchQuery: PropTypes.string.isRequired,
   page: PropTypes.number.isRequired,
   getLargeImg: PropTypes.func,
-  // isLoading: PropTypes.func.isRequired,
   handleGetError: PropTypes.func.isRequired,
   handleButtonHide: PropTypes.func.isRequired,
 };
